11390f88675bc270c52d0e4eb1ee6eaa539696b5
GH <geonhee619@gmail.com>
2024-12-27 01:06:24 +0900

    Initial

---
+++ Init.jl
@@ -0,0 +1,21 @@
+using DataFrames, CSV, JSON, JLD, RData
+using Dates, LaTeXStrings, Serialization
+using LinearAlgebra, SparseArrays, NamedArrays
+using Random, Distributions, StatsBase, StatsFuns
+using Plots, StatsPlots, ProgressMeter
+using AdvancedHMC, LogDensityProblems
+using BridgeStan, PSIS, Roots
+using FLoops#, CUDA, FoldsCUDA
+
+vecvec2mat(_v) = reduce(hcat, _v)' |> Matrix
+mat2vecvec(_m) = [_m[i,:] for i in 1:size(_m,1)]
+symmetric(_m) = Symmetric((_m + _m') ./ 2)
+_round(_x::Float64)::Float64 = round(_x; digits=3)
+_sum(_M; dims::Int64) = dropdims(sum(_M; dims=dims); dims=dims)
+_mean(_M; dims::Int64) = dropdims(mean(_M; dims=dims); dims=dims)
+
+default(size=(500,200))
+
+(^)(f::Function, i::Int) = i==1 ? f : x->(f^(i-1))(f(x))
+
+;
---
+++ LEO.jl
@@ -0,0 +1,197 @@
+Base.@kwdef mutable struct Params
+    β::Matrix{Float64} = zeros(data.T, 3)
+    β_0::Vector{Float64} = zeros(3)
+    Σ_β::Matrix = I(3)
+    Σ_y::Matrix = I(data.τ_axis |> length)
+end
+
+function Σy_gibbs(c::Params)::Params
+    
+    ν_0, S_0 = 2data.K, Matrix(I, data.K,data.K)
+    
+    E = Vector(undef, data.T)
+    t = 1
+    for t in eachindex(E)
+        ŷ = data.X * c.β[t,:]
+        e = data.y[t,:] - ŷ
+        E[t] = e * e'
+    end
+    Σ_y_sample = InverseWishart(ν_0 + length(E), S_0 + sum(E)) |> rand
+    
+    c.Σ_y = Σ_y_sample
+    c
+end
+
+function Σβ_gibbs(c::Params)::Params
+    
+    K = 3
+    ν_0, S_0 = 2K, Matrix(I, K, K)
+    
+    E = Vector(undef, data.T)
+    e = c.β[1,:] - c.β_0
+    E[1] = e * e'
+    for t in 2:data.T
+        e = c.β[t,:] - c.β[t-1,:]
+        E[t] = e * e'
+    end
+    Σ_β_sample = InverseWishart(
+        ν_0 + length(E),
+        S_0 + sum(E)
+    ) |> rand
+
+    c.Σ_β = Σ_β_sample
+    c
+end
+
+function β0_gibbs(c::Params)::Params
+    
+    m_0, J_0 = zeros(3), I(3)/10
+    h_0 = J_0 * m_0
+    
+    Σ_β_inv = inv(c.Σ_β)
+    β_0_sample = MvNormalCanon(
+        h_0 + Σ_β_inv * c.β[1,:],
+        (J_0 + Σ_β_inv) |> symmetric
+    ) |> rand
+    
+    c.β_0 = β_0_sample
+    c
+end
+
+function β_ffbs(c::Params)::Params
+    
+    K, ϵ = 3, 1e-10
+    
+    """Filtering"""
+    # initialize state moments
+    θₜˌₜ_mean = zeros(data.T, K) # filtered state variable
+    θₜˌₜ_cov  = repeat([zeros(K, K)], data.T) # variance
+    θₜˌₜ      = c.β_0
+    Σ_θₜˌₜ    = c.Σ_β
+    G        = I(K)
+    F        = I(K)'
+    
+    for t in 1:data.T
+        # State prior
+        θₜˌₜ₋₁ = G * θₜˌₜ # transition
+        Σ_θₜˌₜ₋₁ = G * Σ_θₜˌₜ * G' + c.Σ_β
+        # 1-step prediction
+        F = data.X'
+        fₜ = F' * θₜˌₜ₋₁ # measurement
+        Qₜ = F' * Σ_θₜˌₜ₋₁ * F + c.Σ_y
+        eₜ = data.y[t,:] - fₜ
+        # State posterior moments
+        θₜˌₜ = θₜˌₜ₋₁ + (Σ_θₜˌₜ₋₁ * F * (Qₜ \ eₜ))
+        Σ_θₜˌₜ = Σ_θₜˌₜ₋₁ - (Σ_θₜˌₜ₋₁ * F) * (Σ_θₜˌₜ₋₁ * (F / Qₜ))'
+        # Store
+        θₜˌₜ_mean[t,:] = θₜˌₜ
+        θₜˌₜ_cov[t] = Σ_θₜˌₜ
+    end
+    
+    """Sampling"""
+    m = zeros(data.T, K); m[data.T,:] = θₜˌₜ_mean[data.T,:]
+    C = repeat([zeros(K, K)], data.T); C[data.T] = θₜˌₜ_cov[data.T]
+    θ_sample = zeros(data.T, K)
+    θ_sample[data.T,:] = MvNormal(m[data.T,:], (C[data.T] + ϵ*I) |> symmetric) |> rand
+    for t in data.T-1:-1:1
+        θₜ₊₁ˌₜ = G * θₜˌₜ_mean[t,:]
+        Σ_θₜ₊₁ˌₜ = G * θₜˌₜ_cov[t] * G' + c.Σ_β
+        m[t,:] = θₜˌₜ_mean[t,:] + (θₜˌₜ_cov[t] * G' * (Σ_θₜ₊₁ˌₜ \ (θ_sample[t+1,:] - θₜ₊₁ˌₜ)))
+        C[t] = θₜˌₜ_cov[t] - (θₜˌₜ_cov[t] * G' * (θₜˌₜ_cov[t] * (G' / Σ_θₜ₊₁ˌₜ))')
+        θ_sample[t,:] = MvNormal(m[t,:], (C[t] + ϵ*I) |> symmetric) |> rand
+    end
+    
+    c.β = θ_sample
+    c
+end
+
+function Σy_gibbs_LEO(c::Params)::Params
+    
+    T, ν_0, S_0 = data.T+(data.ρ == 0 ? -1 : 0), 2data.K, Matrix(I, data.K,data.K)
+    
+    E = Vector(undef, T)
+    t = 1
+    for t in eachindex(E)
+        ŷ = data.X * c.β[t,:]
+        e = data.y[t,:] - ŷ
+        e = e * (t == data.T ? sqrt(data.ρ) : 1)
+        E[t] = e * e'
+    end
+    Σ_y_sample = InverseWishart(ν_0 + length(E), S_0 + sum(E)) |> rand
+    
+    c.Σ_y = Σ_y_sample
+    c
+end
+
+function Σβ_gibbs_LEO(c::Params)::Params
+    
+    K = 3
+    T, ν_0, S_0 = data.T+(data.ρ == 0 ? -1 : 0), 2K, Matrix(I, K, K)
+    
+    E = Vector(undef, T)
+    e = c.β[1,:] - c.β_0
+    E[1] = e * e'
+    for t in 2:T
+        e = c.β[t,:] - c.β[t-1,:]
+        E[t] = e * e'
+    end
+    Σ_β_sample = InverseWishart(
+        ν_0 + length(E),
+        S_0 + sum(E)
+    ) |> rand
+
+    c.Σ_β = Σ_β_sample
+    c
+end
+
+function β_ffbs_LEO(c::Params)::Params
+    
+    T, K, ϵ = data.T+(data.ρ == 0 ? -1 : 0), 3, 1e-10
+    
+    """Filtering"""
+    # initialize state moments
+    θₜˌₜ_mean = zeros(data.T, K) # filtered state variable
+    θₜˌₜ_cov  = repeat([zeros(K, K)], data.T) # variance
+    θₜˌₜ      = c.β_0
+    Σ_θₜˌₜ    = c.Σ_β
+    G        = I(K)
+    F        = I(K)'
+    
+    for t in 1:T
+        # State prior
+        θₜˌₜ₋₁ = G * θₜˌₜ # transition
+        Σ_θₜˌₜ₋₁ = G * Σ_θₜˌₜ * G' + c.Σ_β
+        # 1-step prediction
+        F = data.X'
+        fₜ = F' * θₜˌₜ₋₁ # measurement
+        Qₜ = F' * Σ_θₜˌₜ₋₁ * F + c.Σ_y / (t == data.T ? data.ρ : 1)
+        eₜ = data.y[t,:] - fₜ
+        # State posterior moments
+        θₜˌₜ = θₜˌₜ₋₁ + (Σ_θₜˌₜ₋₁ * F * (Qₜ \ eₜ))
+        Σ_θₜˌₜ = Σ_θₜˌₜ₋₁ - (Σ_θₜˌₜ₋₁ * F) * (Σ_θₜˌₜ₋₁ * (F / Qₜ))'
+        # Store
+        θₜˌₜ_mean[t,:] = θₜˌₜ
+        θₜˌₜ_cov[t] = Σ_θₜˌₜ
+    end
+    
+    """Sampling"""
+    m = zeros(data.T, K); m[T,:] = θₜˌₜ_mean[T,:]
+    C = repeat([zeros(K, K)], data.T); C[T] = θₜˌₜ_cov[T]
+    θ_sample = zeros(data.T, K)
+    θ_sample[T,:] = MvNormal(m[T,:], (C[T] + ϵ*I) |> symmetric) |> rand
+    for t in T-1:-1:1
+        θₜ₊₁ˌₜ = G * θₜˌₜ_mean[t,:]
+        Σ_θₜ₊₁ˌₜ = G * θₜˌₜ_cov[t] * G' + c.Σ_β
+        m[t,:] = θₜˌₜ_mean[t,:] + (θₜˌₜ_cov[t] * G' * (Σ_θₜ₊₁ˌₜ \ (θ_sample[t+1,:] - θₜ₊₁ˌₜ)))
+        C[t] = θₜˌₜ_cov[t] - (θₜˌₜ_cov[t] * G' * (θₜˌₜ_cov[t] * (G' / Σ_θₜ₊₁ˌₜ))')
+        θ_sample[t,:] = MvNormal(m[t,:], (C[t] + ϵ*I) |> symmetric) |> rand
+    end
+    
+    c.β = θ_sample
+    c
+end
+
+GibbsScan = β_ffbs ∘ β0_gibbs ∘ Σβ_gibbs ∘ Σy_gibbs
+GibbsScan_LEO = β_ffbs_LEO ∘ β0_gibbs ∘ Σβ_gibbs_LEO ∘ Σy_gibbs_LEO
+
+;
